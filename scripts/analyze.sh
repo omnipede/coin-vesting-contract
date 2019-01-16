ddc0b488ce5b14bed674070226bdc7e2c68d2810
Eric Shin <ywshin@coinplug.com>
2019-01-16 10:50:39 +0900

    update script file

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -6,9 +6,13 @@ mkdir -p reports
 npm run flatten
 
 echo "Analyzing contracts with Mythril..."
+# example
+# docker run -v $(pwd):/metadium -v /tmp/myth/solc-v0.4.24:/root/.py-solc/solc-v0.4.24 -w "/metadium/" mythril/myth:latest --solv 0.4.24 -x flat/{sol File} -o markdown >> reports/{output file}
+docker run -v $(pwd):/metadium -v /tmp/myth/solc-v0.4.24:/root/.py-solc/solc-v0.4.24 -w "/metadium/" mythril/myth:latest --solv 0.4.24 -x flat/AdminAnchor.sol -o markdown >> reports/AdminAnchor_Mythrill.md
+docker run -v $(pwd):/metadium -v /tmp/myth/solc-v0.4.24:/root/.py-solc/solc-v0.4.24 -w "/metadium/" mythril/myth:latest --solv 0.4.24 -x flat/Ballot.sol -o markdown >> reports/Ballot_Mythrill.md
 
-#myth -x flat_contracts/AdminAnchor.sol -o markdown >> reports/AdminAnchor_Mythrill.md
-#docker run -v $(pwd):/metadium -w "/metadium/" mythril/myth:latest --solv 0.4.24 -x flat/AdminAnchor.sol >> reports/AdminAnchor_Mythrill.md
-docker run -v $(pwd):/metadium -v /tmp/myth/solc-v0.4.24:/root/.py-solc/solc-v0.4.24 -w "/metadium/" mythril/myth:latest --solv 0.4.24 -x flat/AdminAnchor.sol
-
-docker run -i -t -v $(pwd):/metadium  toyente python oyente.py -ce -s /metadium/flat/AdminAnchor.sol >> reports/AdminAnchor_Oyente.log
+echo "Analyzing contracts with Oyente..."
+# example
+# docker run -i -t -v $(pwd):/metadium  moyente python oyente.py -ce -s /metadium/flat/{sol File} >> reports/{output file}
+docker run -i -t -v $(pwd):/metadium  moyente python oyente.py -ce -s /metadium/flat/AdminAnchor.sol >> reports/AdminAnchor_Oyente.log
+docker run -i -t -v $(pwd):/metadium  moyente python oyente.py -ce -s /metadium/flat/Ballot.sol >> reports/Ballot_Oyente.log
--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -6,4 +6,4 @@ mkdir -p flat
 alias flatten="solidity_flattener --solc-paths=\"../=/metadium/contracts/\""
 
 flatten contracts/AdminAnchor.sol --output flat/AdminAnchor.sol
-
+flatten contracts/Ballot.sol --output flat/Ballot.sol
