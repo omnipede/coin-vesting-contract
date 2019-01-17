e29334118bb0866518d64faeeb7c5e7e48d8aa57
hexoul <crosien@gmail.com>
2019-01-17 16:42:45 +0900

    test: imp. framework for test with helpers

---
+++ test/registry.test.js
@@ -0,0 +1,30 @@
+import assertRevert from './helpers/assertRevert'
+import EVMRevert from './helpers/EVMRevert'
+
+const BigNumber = web3.BigNumber
+
+require('chai').use(require('chai-as-promised')).use(require('chai-bignumber')(BigNumber)).should()
+
+const Registry = artifacts.require('Registry.sol')
+
+contract('Registry', function ([deployer]) {
+    let registry
+
+    beforeEach(async () => {
+        registry = await Registry.new()
+    });
+
+    describe('Owner ', function () {
+        beforeEach(async () => {
+
+        });
+        it('can set Contract Domain', async ()=> {
+            assert.equal(true, true)
+        });
+
+        it('can set permission', async ()=> {
+            assert.equal(true, true)
+        });
+    })
+
+});
