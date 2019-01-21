3430a8d08fa071a849110458634a2f1162e09f0a
hexoul <crosien@gmail.com>
2019-01-21 16:39:28 +0900

    staking: imp. lock(), unlock(), availBalance() on contract and test units for that

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -9,6 +9,7 @@ contract Staking is GovChecker, ReentrancyGuard {
     using SafeMath for uint256;
 
     mapping(address => uint256) public balance;
+    mapping(address => uint256) public lockedBalance;
     
     event Staked(address user, uint256 amount, uint256 total);
     event Unstaked(address user, uint256 amount, uint256 total);
@@ -29,12 +30,29 @@ contract Staking is GovChecker, ReentrancyGuard {
         emit Staked(msg.sender, msg.value, balance[msg.sender]);
     }
 
-    function withdraw(uint256 amount) public nonReentrant payable {
-        require(amount <= balance[msg.sender], "Withdraw amount should be equal or less than balance");
+    function withdraw(uint256 amount) external nonReentrant payable {
+        require(amount <= availBalance(msg.sender), "Withdraw amount should be equal or less than balance");
 
         balance[msg.sender] = balance[msg.sender].sub(amount);
         msg.sender.transfer(amount);
 
         emit Unstaked(msg.sender, amount, balance[msg.sender]);
+    }
+
+    function lock(address _addr, uint256 lockAmount) external onlyGov {
+        require(balance[_addr] >= lockAmount, "Lock amount should be less than balance");
+        require(balance[_addr].sub(lockedBalance[_addr]) >= lockAmount, "Insufficient lockable balance");
+
+        lockedBalance[_addr] = lockedBalance[_addr].add(lockAmount);
+    }
+
+    function unlock(address _addr, uint256 unlockAmount) external onlyGov {
+        require(lockedBalance[_addr] >= unlockAmount, "Unlock amount should be less than locked");
+
+        lockedBalance[_addr] = lockedBalance[_addr].sub(unlockAmount);
+    }
+
+    function availBalance(address _addr) public view returns (uint256 availBal) {
+        return balance[_addr].sub(lockedBalance[_addr]);
     }
 }
--- test/staking.test.js
+++ test/staking.test.js
@@ -9,7 +9,7 @@ require('chai')
 const Registry = artifacts.require('Registry.sol');
 const Staking = artifacts.require('Staking.sol');
 
-contract('Staking', function ([deployer, user1, user2]) {
+contract('Staking', function ([deployer, fakeGov, user]) {
   let registry, staking;
   const amount = ether(1);
   
@@ -18,50 +18,80 @@ contract('Staking', function ([deployer, user1, user2]) {
     staking = await Staking.new(registry.address);
 
     await registry.setContractDomain("Staking", staking.address);
+    await registry.setContractDomain("GovernanceContract", fakeGov);
   });
 
   describe('Staker ', function () {
     it('cannot stake zero', async () => {
-      await reverting(staking.deposit({ value: 0, from: user1 }));
+      await reverting(staking.deposit({ value: 0, from: user }));
     });
 
     it('can stake through deposit', async () => {
-      const pre = await ethGetBalance(user1);
-      await staking.deposit({ value: amount, from: user1 });
-      const post = await ethGetBalance(user1);
+      const pre = await ethGetBalance(user);
+      await staking.deposit({ value: amount, from: user });
+      const post = await ethGetBalance(user);
       pre.minus(post).should.be.bignumber.gt(amount);
 
-      const bal = await staking.balance(user1);
+      const bal = await staking.balance(user);
       assert.equal(amount.toNumber(), bal.toNumber());
     });
 
     it('can stake through transfer', async () => {
-      const pre = await ethGetBalance(user2);
-      await staking.sendTransaction({ value: amount, from: user2, gas: 4e6 });
-      const post = await ethGetBalance(user2);
+      const pre = await ethGetBalance(user);
+      await staking.sendTransaction({ value: amount, from: user, gas: 4e6 });
+      const post = await ethGetBalance(user);
       pre.minus(post).should.be.bignumber.gt(amount);
 
-      const bal = await staking.balance(user2);
+      const bal = await staking.balance(user);
       assert.equal(amount.toNumber(), bal.toNumber());
     });
 
     it('cannot withdraw over balance', async () => {
-      await staking.deposit({ value: amount, from: user2 });
-      const bal = await staking.balance(user2);
+      await reverting(staking.withdraw(amount, { from: user }));
+      await staking.deposit({ value: amount, from: user });
+      const bal = await staking.balance(user);
       const bal2x = bal * 2;
-      await reverting(staking.withdraw(bal2x));
+      await reverting(staking.withdraw(bal2x, { from: user }));
     });
 
-    it('can withdraw unlocked balance', async () => {
-      await staking.deposit({ value: amount, from: user2 });
-      const bal = await staking.balance(user2);
-      const pre = await ethGetBalance(user2);
-      await staking.withdraw(bal, { from: user2 });
-      const post = await ethGetBalance(user2);
+    it('can withdraw partial', async () => {
+      await staking.deposit({ value: amount, from: user });
+      const half = amount / 2;
+      await staking.withdraw(half, { from: user });
+      const bal = await staking.balance(user);
+      bal.should.be.bignumber.equal(half);
+    });
+
+    it('can withdraw all', async () => {
+      await staking.deposit({ value: amount, from: user });
+      const bal = await staking.balance(user);
+      const pre = await ethGetBalance(user);
+      await staking.withdraw(bal, { from: user });
+      const post = await ethGetBalance(user);
       post.should.be.bignumber.gt(pre);
 
-      const remain = await staking.balance(user2);
+      const remain = await staking.balance(user);
       remain.should.be.bignumber.equal(0);
+    });
+  });
+
+  describe('Governance ', function () {
+    beforeEach(async () => {
+      await staking.deposit({ value: amount, from: user });
+      await staking.lock(user, amount, { from: fakeGov });
+    });
+
+    it('can lock', async () => {
+      const availBal = await staking.availBalance(user);
+      availBal.should.be.bignumber.equal(0);
+
+      await reverting(staking.withdraw(amount, { from: user }));
+    });
+
+    it('can unlock', async () => {
+      await staking.unlock(user, amount, { from: fakeGov });
+      const availBal = await staking.availBalance(user);
+      availBal.should.be.bignumber.equal(amount);
     });
   });
 
