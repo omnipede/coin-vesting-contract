546d1bda9fd76ff8b41883a802e234547cbe7c2b
hexoul <crosien@gmail.com>
2019-01-21 16:45:46 +0900

    staking: update Events for lock and unlock

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -11,8 +11,10 @@ contract Staking is GovChecker, ReentrancyGuard {
     mapping(address => uint256) public balance;
     mapping(address => uint256) public lockedBalance;
     
-    event Staked(address user, uint256 amount, uint256 total);
-    event Unstaked(address user, uint256 amount, uint256 total);
+    event Staked(address user, uint256 amount, uint256 total, uint256 avail);
+    event Unstaked(address user, uint256 amount, uint256 total, uint256 avail);
+    event Locked(address user, uint256 amount, uint256 total, uint256 avail);
+    event Unlocked(address user, uint256 amount, uint256 total, uint256 avail);
 
     constructor(address _registry) public {
         setRegistry(_registry);
@@ -27,7 +29,7 @@ contract Staking is GovChecker, ReentrancyGuard {
 
         balance[msg.sender] = balance[msg.sender].add(msg.value);
 
-        emit Staked(msg.sender, msg.value, balance[msg.sender]);
+        emit Staked(msg.sender, msg.value, balance[msg.sender], availBalance(msg.sender));
     }
 
     function withdraw(uint256 amount) external nonReentrant payable {
@@ -36,7 +38,7 @@ contract Staking is GovChecker, ReentrancyGuard {
         balance[msg.sender] = balance[msg.sender].sub(amount);
         msg.sender.transfer(amount);
 
-        emit Unstaked(msg.sender, amount, balance[msg.sender]);
+        emit Unstaked(msg.sender, amount, balance[msg.sender], availBalance(msg.sender));
     }
 
     function lock(address _addr, uint256 lockAmount) external onlyGov {
@@ -44,12 +46,16 @@ contract Staking is GovChecker, ReentrancyGuard {
         require(balance[_addr].sub(lockedBalance[_addr]) >= lockAmount, "Insufficient lockable balance");
 
         lockedBalance[_addr] = lockedBalance[_addr].add(lockAmount);
+
+        emit Locked(_addr, lockAmount, balance[_addr], availBalance(_addr));
     }
 
     function unlock(address _addr, uint256 unlockAmount) external onlyGov {
         require(lockedBalance[_addr] >= unlockAmount, "Unlock amount should be less than locked");
 
         lockedBalance[_addr] = lockedBalance[_addr].sub(unlockAmount);
+
+        emit Unlocked(_addr, unlockAmount, balance[_addr], availBalance(_addr));
     }
 
     function availBalance(address _addr) public view returns (uint256 availBal) {
