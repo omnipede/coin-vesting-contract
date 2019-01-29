9bb930e488a78be84fb103f85b0628b6c5195fca
Eric Shin <ywshin@coinplug.com>
2019-01-29 11:27:43 +0900

    Removed unnecessary  test code

--- test/storage/EnvStorage.test.js
+++ test/storage/EnvStorage.test.js
@@ -1,35 +1,9 @@
 /*jslint node: true */
-const ERROR_MSG = 'VM Exception while processing transaction: revert';
 const { reverting } = require('openzeppelin-solidity/test/helpers/shouldFail');
-const { ether } = require('openzeppelin-solidity/test/helpers/ether');
-//const { time } = require('openzeppelin-solidity/test/helpers/time');
 const Registry = artifacts.require('Registry.sol');
-
 const EnvStorage = artifacts.require("EnvStorage.sol");
 const EnvStorageImp = artifacts.require("EnvStorageImp.sol");
-const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-
-const moment = require('moment');
-var web3Utils = require('web3-utils');
-var web3EthAbi = require('web3-eth-abi');
 const truffleAssert = require('truffle-assertions');
-
-// For each byte in our array, retrieve the char code value of the binary value
-const binArrayToString = array => array.map(byte => String.fromCharCode(parseInt(byte, 2))).join('')
-
-// Basic left pad implementation to ensure string is on 8 bits
-const leftPad = str => str.length < 8 ? (Array(8).join('0') + str).slice(-8) : str
-
-// For each char of the string, get the int code and convert it to binary. Ensure 8 bits.
-const stringToBinArray = str => str.split('').map(c => leftPad(c.charCodeAt().toString(2)))
-function hex2a(hexx) {
-    var hex = hexx.toString();//force conversion
-    var str = '';
-    for (var i = 0; (i < hex.length && hex.substr(i, 2) !== '00'); i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
-
 
 require('chai')
   .use(require('chai-as-promised'))
@@ -74,8 +48,7 @@ contract('EnvStorage', accounts => {
                 return ev._name === web3.sha3("blockPer") && ev._value === BLOCK_PER_VALUE;
             });
             const [_type, _value] = await testEnvStorage.getBlockPer();
-            
-            console.log(`BlockPer : ${_type}, ${_value}`);
+
             _type.should.be.bignumber.equal(2);
             assert.equal(_value,BLOCK_PER_VALUE);
             const _ttype = await testEnvStorage.getBlockPerType();
@@ -93,7 +66,7 @@ contract('EnvStorage', accounts => {
             //console.log(`BlockPer : ${_type}, ${_value}`);
             _type.should.be.bignumber.equal(2);
             assert.equal(_value,BLOCK_PER_VALUE);
-            
+
             //Update BlockPer variable
             _result = await testEnvStorage.setBlockPer(UPDATE_BLOCK_PER_VALUE, { value: 0, from: govAddr });
             truffleAssert.eventEmitted(_result, 'VarableChanged', (ev) => {
